3ee2bdc89d6c74811dc8255a3885ef5c17a35a21
Daniel Seres <36477258+seresdani@users.noreply.github.com>
2024-04-16 14:30:31 +0200

    generate colors

--- src/theme.tsx
+++ src/theme.tsx
@@ -2,29 +2,41 @@ import { createTheme, rgbToHex } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
 // A custom theme for this app
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#60a425',
+const theme = createTheme(theme, (() => {
+  console.log(
+    theme.palette.augmentColor({
+      color: {
+        main: '#62A527',
+      },
+      name: 'cropster-green',
+    })
+  )
+
+  return {
+    palette: {
+      primary: {
+        main: '#62A527',
+      },
+      secondary: {
+        main: '#C5D3D8',
+      },
+      error: {
+        main: red.A400,
+      },
     },
-    secondary: {
-      main: '#282a2b',
+    typography: {
+      subtitle1: {
+        fontSize: 12,
+      },
+      body1: {
+        fontWeight: 500,
+      },
+      // button: {
+      //   fontStyle: 'italic',
+      // },
     },
-    error: {
-      main: red.A400,
-    },
-  },
-  typography: {
-    subtitle1: {
-      fontSize: 12,
-    },
-    body1: {
-      fontWeight: 500,
-    },
-    // button: {
-    //   fontStyle: 'italic',
-    // },
-  },
-});
+  }
+})());
 
 export default theme;
+
